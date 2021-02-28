53819afc682f7ca5062c83d41c4087655b655299
Jaime <jaimetravesedo125@gmail.com>
2021-02-28 18:07:49 +0100

    feat: get tweets and save them

--- twitter/get_data.py
+++ twitter/get_data.py
@@ -11,8 +11,16 @@ def get_data():
 
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+    name = 'tendobrainrot'
     tweet_id = '1364643149457592320'
 
-    tweet = api.get_status(tweet_id)
-    print(tweet.text)
-    return
+    replies = []
+    for tweet in tweepy.Cursor(api.search, q='to:' + name, result_type='recent', timeout=999999).items(10):
+        if hasattr(tweet, 'in_reply_to_status_id_str'):
+            if tweet.in_reply_to_status_id_str == tweet_id:
+                replies.append(tweet.text[15:])
+
+    with open("twitter/data.txt", "w") as f:
+        f.write('\n'.join(replies))
+
+    return replies
