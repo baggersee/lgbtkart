233cc6e3bf5cc7dd3cfef793c4bb288cafb499cb
Jaime <jaimetravesedo125@gmail.com>
2021-03-01 13:22:09 +0100

    feat: clean data using regex

--- twitter/get_data.py
+++ twitter/get_data.py
@@ -1,8 +1,9 @@
 import tweepy
 import json
+import re
 
 
-def get_replies():
+def get_replies(n):
     with open("twitter/keys.json", "r") as infile:
         api_key, api_skey, access_token, access_stoken = json.load(infile).values()
 
@@ -14,14 +15,22 @@ def get_replies():
     name = 'tendobrainrot'
     tweet_id = '1364643149457592320'
 
-    n_tries = 1000
     replies = []
-    for tweet in tweepy.Cursor(api.search, q='to:' + name, result_type='recent', timeout=999999).items(n_tries):
+    for tweet in tweepy.Cursor(api.search, q='to:' + name, result_type='recent', timeout=999999).items(n):
         if hasattr(tweet, 'in_reply_to_status_id_str'):
             if tweet.in_reply_to_status_id_str == tweet_id:
-                replies.append(tweet.text[15:])
+                replies.append(clean_text(tweet.text))
 
     with open("twitter/replies.txt", "w") as f:
         f.write('\n'.join(replies))
 
     return replies
+
+
+def clean_text(text):
+    text = re.sub(r'@[A-Za-z0-9_]+', '', text)
+    text = re.sub(r"[^\u0000-\u007F]+", "", text)
+    text = re.sub(r'RT[\s]+', '', text)
+    text = re.sub(r'https?:\/\/\S+', '', text)
+    text = text.lower()
+    return text
