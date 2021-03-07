9846665b2770d52bcb7b89c143144ebe8f80b23e
baggersee <79531778+baggersee@users.noreply.github.com>
2021-03-07 16:57:16 +0100

    Add files via upload
    brute force

---
+++ twitter/keywords.py
@@ -0,0 +1,57 @@
+
+# SEXUAL ORIENTATION: hetero,gay ,lesbian,bisexual, asexual
+# we take into account that there are several ways to refer to a certain orientation
+
+
+
+
+heteros = ['straight','hetero']
+
+gays = ['gay','homo']
+
+lesbians = ['lesbian']
+
+bisexuals = ['bisexual','pansexual','bi']
+
+asexuals = ['asexual']
+
+orientations_list = [heteros,gays,lesbians,bisexuals,asexuals]
+
+
+# CHARACTERS
+# for the moment, just a few
+
+
+mario = ['mario']
+
+luigi = ['luigi']
+
+peach = ['peach','princess peach']
+
+daisy = ['daisy','princess daisy']
+
+rosalina = ['rosalina']
+
+tanooki =['tanooki mario','tanooki']
+
+cat = ['cat peach']
+
+yoshi = ['yoshi']
+
+toad = ['toad']
+
+koopa = ['koopa troopa','koopa','troopa']
+
+shy = ['shy guy','shy']
+
+
+
+characters_list = [mario, luigi, peach, daisy,rosalina,tanooki,cat,yoshi,koopa,shy]
+                   
+                  
+
+
+
+
+
+ 
