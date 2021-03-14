16fd6f32864b48bf2dac28dd8d870ed14050287d
Jaime Travesedo <jaimetravesedo125@gmail.com>
2021-03-14 17:30:12 +0100

    updated for adding import

--- detection.py
+++ detection.py
@@ -1,10 +1,12 @@
+from string import ascii_lowercase as letters
+
 def detection(sentence, keyword):
 	sentence = " " + sentence + " "
 	ind = sentence.find(keyword)
 
 	if ind == -1 or 
-	   sentence[ind-1] in string.ascii_lowercase or
-	   sentence[ind + len(keyword)] in string.ascii_lowercase:
+	   sentence[ind-1] in letters or
+	   sentence[ind + len(keyword)] in letters:
 		return False
 	
 	return True
